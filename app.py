0b64bf96516ab9ef7708ed073b8c85b33a44df9a
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-24 21:35:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,35 +3,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import json
 from pandas import json_normalize
+import os
 
-# Function to load data from stock data folder
-def load_data():
-    # Replace 'path/to/stock_data/folder' with the actual path to your stock data folder
-    folder_path = 'stock_data'
+# Function to load data for a specific stock
+def load_data(stock_name, folder_path):
+    file_path = os.path.join(folder_path, f'{stock_name}.json')
 
-    # Load balance sheet and income statement data
-    with open(f'{folder_path}/balance_sheet.json') as f:
-        balance_sheet_data = json.load(f)
+    if os.path.exists(file_path):
+        with open(file_path) as f:
+            stock_data = json.load(f)
 
-    with open(f'{folder_path}/income_statement.json') as f:
-        income_statement_data = json.load(f)
+        # Assuming 'balance_sheet' and 'income_statement' are keys in the JSON file
+        balance_sheet_data = stock_data.get('balance_sheet', {})
+        income_statement_data = stock_data.get('income_statement', {})
 
-    # Convert JSON data to Pandas DataFrame
-    balance_sheet = json_normalize(balance_sheet_data)
-    income_statement = json_normalize(income_statement_data)
+        # Convert JSON data to Pandas DataFrame
+        balance_sheet = json_normalize(balance_sheet_data)
+        income_statement = json_normalize(income_statement_data)
 
-    return balance_sheet, income_statement
+        return balance_sheet, income_statement
+    else:
+        st.warning(f"Data not found for stock: {stock_name}")
+        return None, None
 
 # Function to display tables
-def display_tables(balance_sheet, income_statement):
-    st.subheader('Balance Sheet')
+def display_tables(balance_sheet, income_statement, stock_name):
+    st.subheader(f'{stock_name} - Balance Sheet')
     st.dataframe(balance_sheet)
 
-    st.subheader('Income Statement')
+    st.subheader(f'{stock_name} - Income Statement')
     st.dataframe(income_statement)
 
 # Function to generate and display a chart
-def generate_chart(balance_sheet, income_statement):
+def generate_chart(balance_sheet, income_statement, stock_name):
     # Replace 'column_name' with the column you want to use for the chart
     column_name = 'example_column'
 
@@ -42,23 +46,33 @@ def generate_chart(balance_sheet, income_statement):
 
     plt.xlabel('Date')
     plt.ylabel(column_name)
-    plt.title(f'Balance Sheet and Income Statement: {column_name}')
+    plt.title(f'{stock_name} - Balance Sheet and Income Statement: {column_name}')
     plt.legend()
 
-    st.subheader('Chart')
+    st.subheader(f'{stock_name} - Chart')
     st.pyplot()
 
 def main():
     st.title('Stock Data Analysis App')
 
-    # Load data
-    balance_sheet, income_statement = load_data()
+    # Replace 'path/to/stock_data/folder' with the actual path to your stock data folder
+    folder_path = 'stock_data'
 
-    # Display tables
-    display_tables(balance_sheet, income_statement)
+    # Get the list of stock files in the folder
+    stock_files = [file.split('.')[0] for file in os.listdir(folder_path) if file.endswith('.json')]
 
-    # Generate and display chart
-    generate_chart(balance_sheet, income_statement)
+    # Allow the user to select a stock
+    selected_stock = st.selectbox('Select a stock', stock_files)
+
+    # Load data for the selected stock
+    balance_sheet, income_statement = load_data(selected_stock, folder_path)
+
+    if balance_sheet is not None and income_statement is not None:
+        # Display tables
+        display_tables(balance_sheet, income_statement, selected_stock)
+
+        # Generate and display chart
+        generate_chart(balance_sheet, income_statement, selected_stock)
 
 if __name__ == '__main__':
     main()
